39e9c0f6c6e51b65d64c97634a0cc484e3597b4b
Carolline Martins <106552762+carollinemv@users.noreply.github.com>
2023-02-09 15:52:56 -0300

    Add files via upload

--- Robô da tabuada/tabuada.js
+++ Robô da tabuada/tabuada.js
@@ -5,6 +5,8 @@ const numero = prompt(
 
 let resultado = ""
 
-for(let fator = 1; fator <=20; fator++){
+for(let fator = 1; fator <= 20; fator++){
     resultado += " -> " + numero + " * " + fator + " = " + (numero * fator) + "\n"
 }
+
+alert("Resultado da tabuada de " + numero + ":\n\n" + resultado)
